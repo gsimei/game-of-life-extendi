1edb29d3783ecc035edac4fa5a42f998c0f6dc15
George Simei <georgesimei@gmail.com>
2024-12-09 18:03:12 +0100

    Fixed the tests for the game states controller

--- test/controllers/game_states_controller_test.rb
+++ test/controllers/game_states_controller_test.rb
@@ -2,18 +2,19 @@ require "test_helper"
 
 class GameStatesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @game_state = game_states(:one)
     @user = users(:one)
     sign_in @user
+
+    @game_state = game_states(:one)
   end
 
   test "should get index" do
-    get game_states_index_url
+    get game_states_url
     assert_response :success
   end
 
   test "should get show" do
-    get game_states_show_url
+    get game_state_url(@game_state)
     assert_response :success
   end
 end
