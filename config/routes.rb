f6e84eb11f0833dddf752022599f8244abc2978a
George Simei <georgesimei@gmail.com>
2024-12-09 15:42:29 +0100

    Created the home controller and view index

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "home#index"
   devise_for :users
+  root "home#index"
 end
