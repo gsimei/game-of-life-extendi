c8e9768d3e205e4735acced27cf3671e16670e23
George Simei <georgesimei@gmail.com>
2025-02-11 11:19:58 +0100

    Included the routes for the API version 1 and the game_states controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@
 #     # controller actions here
 #   end
 #
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
